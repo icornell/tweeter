4b8b6dd7d978ac0ed340ae164450b26b4a5150e2
India Cornell <indiaccornell@gmail.com>
2023-08-19 17:55:14 -0700

    Remove .counter and update for counterElement to traverse the DOM with ancestor elements

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -6,16 +6,18 @@ $(document).ready(function () {
     const inputValue = $(this).val();
     const remainingChars = maxLength - inputValue.length;
 
-    //Update the character counter text
-    $(".counter").text(remainingChars);
+    // Find the closest .new-tweet section and then find the .counter element within it
+    const counterElement = $(this).closest('.new-tweet').find('.counter');
+
+    counterElement.text(remainingChars);
 
     //Check if the remaining characters are negative or positive (under or over the 140 char. limit)
     if (remainingChars < 0) {
-      $(".counter").addClass("over-limit"); //Add red class
-      $(".counter").removeClass("under-limit"); //Remove green class
+      counterElement.addClass("over-limit"); //Add red class
+      counterElement.removeClass("under-limit"); //Remove green class
     } else {
-      $(".counter").removeClass("over-limit"); //Remove red class
-      $(".counter").addClass("under-limit"); //Add green class
+      counterElement.removeClass("over-limit"); //Remove red class
+      counterElement.addClass("under-limit"); //Add green class
     }
   });
 });
