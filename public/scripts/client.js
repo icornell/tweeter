518e054dbf5cca0addf6eeda0c5d3fcee687a7a4
India Cornell <indiaccornell@gmail.com>
2023-08-30 20:28:04 -0700

    Update client.js for loadTweet Function - needs debugging

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -29,6 +29,37 @@ $(document).ready(function () {
       created_at: 1461113959088,
     },
   ];
+
+  //function to create a tweet element
+  const createTweetElement = function (tweet) {
+    let $tweet = $(`
+      <article class="tweet">
+      <header class="tweet-header">
+        <img class="avatar" src="${tweet.user.avatars}" alt="User Profile Image">
+        <div class="user-info">
+          <h3 class="name">${tweet.user.name}</h3>
+          <p class="handle">${tweet.user.handle}</p>
+        </div>
+      </header>
+      <div class="tweet-content">
+        <p>${tweet.content.text}</p>
+      </div>
+      <hr class="tweet-line">
+      <footer class="tweet-footer">
+        <div class="date">
+          <p class="timestamp">${tweet.created_at}</p> 
+        </div>
+        <div class="icons">
+          <i class="fa-solid fa-comment"></i>
+          <i class="fas fa-retweet"></i>
+          <i class="fa-solid fa-heart"></i>
+        </div>
+      </footer>
+    </article>
+        `);
+    return $tweet;
+  };
+
   //function to render the tweets
   const renderTweets = function (tweets) {
     const $tweetsContainer = $("#tweets-container");
@@ -37,47 +68,31 @@ $(document).ready(function () {
       $tweetsContainer.append($tweetElement);
     }
   };
-
-  //function to create a tweet element NOTE RETURN HERE FOR TIMESTAMP
-  const createTweetElement = function (tweet) {
-    let $tweet = $(`
-    <article class="tweet">
-    <header class="tweet-header">
-      <img class="avatar" src="${tweet.user.avatars}" alt="User Profile Image">
-      <div class="user-info">
-        <h3 class="name">${tweet.user.name}</h3>
-        <p class="handle">${tweet.user.handle}</p>
-      </div>
-    </header>
-    <div class="tweet-content">
-      <p>${tweet.content.text}</p>
-    </div>
-    <hr class="tweet-line">
-    <footer class="tweet-footer">
-      <div class="date">
-        <p class="timestamp">${tweet.created_at}</p> 
-      </div>
-      <div class="icons">
-        <i class="fa-solid fa-comment"></i>
-        <i class="fas fa-retweet"></i>
-        <i class="fa-solid fa-heart"></i>
-      </div>
-    </footer>
-  </article>
-      `);
-    return $tweet;
-  };
-
+//call the renderTweets function with the data
   renderTweets(data);
+
+  const loadTweets = function () {
+    //deifne a function to fetch the tweets using AJAX
+    $.get("http://localhost:8080/tweets")
+      .then(function (response) {
+        // Call the renderTweets function with the response
+        renderTweets(response);
+      })
+      .catch(function (error) {
+        console.error("Error fetching tweets:", error);
+      });
+  };
+  // Call the loadTweets function to fetch and render tweets on page load
+  loadTweets();
 
   // Add an event listener for the form submit event
   $(".new-tweet-form").submit(function (event) {
     console.log("Form submitted");
     // Prevent the default form submission behavior
     event.preventDefault();
-    // Serialize the form data
+    //serialize the form data
     const serializedData = $(this).serialize();
-
+    //send a POST request for a new tweet
     $.post("/tweets", serializedData)
       .then((response) => {
         console.log("Success: ", response);
